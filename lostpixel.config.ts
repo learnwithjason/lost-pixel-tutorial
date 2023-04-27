a9c8e99efefc4c303907c8ad339a208719fe23ee
Jason Lengstorf <jason@lengstorf.com>
2023-04-27 10:27:22 -0700

    feat: add the Lost Pixel GitHub action

---
+++ lostpixel.config.ts
@@ -0,0 +1,10 @@
+import { type CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+  pageShots: {
+    pages: [{ path: "/", name: "landing" }],
+    baseUrl: "http://172.17.0.1:3000",
+  },
+  lostPixelProjectId: "clgze4lua005in90e5bqe62ck",
+  apiKey: "7bbe1822-cf7d-4654-af08-6946e178eecb",
+};
